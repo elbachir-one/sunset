ad0dcdc69502a557540d6381240870307744a9c0
elbachir-one <bachiralfa@gmail.com>
2024-06-28 15:41:09 +0100

    No js ever

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard(color) {
-    const el = document.createElement('textarea');
-    el.value = color;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    alert(`Copied ${color} to clipboard!`);
-}
