97b13dea7bd9d677143b85ecf05eb0c9c458ab07
elbachir-one <bachiralfa@gmail.com>
2024-06-28 12:49:58 +0100

    Adding copy color to the clipboard

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function copyToClipboard(color) {
+    const el = document.createElement('textarea');
+    el.value = color;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    alert(`Copied ${color} to clipboard!`);
+}
